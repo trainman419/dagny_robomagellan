6a2894a62f68a6f25678ebe34af74c1494571e73
trainman419 <legotown@aol.com>
2014-06-08 23:51:46 -0700

    Initial implementation of relative GPS publisher

--- utm_tf_publisher/src/utm_tf_publisher.cpp
+++ utm_tf_publisher/src/utm_tf_publisher.cpp
@@ -38,12 +38,88 @@
 
 #include <ros/ros.h>
 #include <sensor_msgs/NavSatFix.h>
+#include <nav_msgs/Odometry.h>
 
 #include <geodesy/wgs84.h>
 #include <geodesy/utm.h>
 
-void fixCallback(const sensor_msgs::NavSatFix::ConstPtr & msg) {
+class NavSatTfPub {
+  public:
+    NavSatTfPub();
+
+  private:
+    void fixCallback(const sensor_msgs::NavSatFix::ConstPtr & msg);
+
+    ros::NodeHandle nh_;
+    ros::NodeHandle pnh_;
+    ros::Subscriber fix_sub_;
+    ros::Publisher odom_pub_;
+
+    geodesy::UTMPoint initial_point_;
+    bool initial_point_valid_;
+    bool relative_;
+
+    std::string frame_id_;
+    bool override_frame_id_;
+
+    std::string child_frame_id_;
+    bool override_child_frame_id_;
+};
+
+NavSatTfPub::NavSatTfPub() : pnh_("~"), initial_point_valid_(false) {
+  fix_sub_ = nh_.subscribe("fix", 10, &NavSatTfPub::fixCallback, this);
+  odom_pub_ = nh_.advertise<nav_msgs::Odometry>("odom", 10);
+  pnh_.param<bool>("relative", relative_, false);
+
+  override_frame_id_ = pnh_.getParam("frame_id", frame_id_);
+  pnh_.param<std::string>("child_frame_id", child_frame_id_, "base_link");
+}
+
+void NavSatTfPub::fixCallback(const sensor_msgs::NavSatFix::ConstPtr & msg) {
+  geographic_msgs::GeoPoint geo_point = geodesy::toMsg(*msg);
+  geodesy::UTMPoint utm_point(geo_point);
+
+  if(!initial_point_valid_) {
+    initial_point_ = utm_point;
+    initial_point_valid_ = true;
+  }
+
+  if(relative_) {
+    if(utm_point.zone == initial_point_.zone
+        && utm_point.band == initial_point_.band) {
+      utm_point.altitude -= initial_point_.altitude;
+      utm_point.northing -= initial_point_.northing;
+      utm_point.easting  -= initial_point_.easting;
+    } else {
+      ROS_ERROR("Initial UTM zone and current zone are not the same!");
+      // TODO: do something more clever here...
+    }
+  }
+
+  nav_msgs::Odometry odom;
+  odom.header = msg->header;
+  if(override_frame_id_) {
+    odom.header.frame_id = frame_id_;
+  }
+  odom.child_frame_id = child_frame_id_;
+  odom.pose.pose.position.x = utm_point.easting;
+  odom.pose.pose.position.y = utm_point.northing;
+  odom.pose.pose.position.z = utm_point.altitude;
+
+  odom.pose.pose.orientation.x = 0;
+  odom.pose.pose.orientation.y = 0;
+  odom.pose.pose.orientation.z = 0;
+  odom.pose.pose.orientation.w = 1;
+  for( int i=0; i<3; i++ ) {
+    for(int j=0; j<3; j++ ) {
+      odom.pose.covariance[i*6+j] = msg->position_covariance[i*3+j];
+    }
+  }
+  odom_pub_.publish(odom);
 }
 
 int main(int argc, char ** argv) {
+  ros::init(argc, argv, "utm_tf_pub");
+  NavSatTfPub pub;
+  ros::spin();
 }
